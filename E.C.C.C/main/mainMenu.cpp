201d4cd2c97546e38ae2f5e953cb3115009fd74e
BRMilev22 <BRMilev22@codingburgas.bg>
2024-02-03 01:21:13 +0200

    Add buttons to main menu #21 +  Add hover effect for buttons in main menu #22

--- E.C.C.C/main/mainMenu.cpp
+++ E.C.C.C/main/mainMenu.cpp
@@ -47,10 +47,13 @@ void drawmainMenu(gameState& gameState)
     Image imScarfyAnim = LoadImageAnim("../assets/bg.gif", &animFrames);
 
     Texture2D texScarfyAnim = LoadTextureFromImage(imScarfyAnim);
-    Texture2D buttonPlayIdle = LoadTexture("../assets/playidle.png"); 
-    Texture2D buttonSettingsIdle = LoadTexture("../assets/settingsIdle.png");
-    Texture2D buttonCreditsIdle = LoadTexture("../assets/creditsIdle.png");
-    Texture2D buttonQuitIdle = LoadTexture("../assets/quitIdle.png");
+    Texture2D buttonPlayIdle = LoadTexture("../assets/buttons/play/playidle.png");
+    Texture2D buttonPlayHover = LoadTexture("../assets/buttons/play/playhover.png");
+    Texture2D buttonSettingsHover = LoadTexture("../assets/buttons/settings/settingsHover.png");
+    Texture2D buttonSettingsIdle = LoadTexture("../assets/buttons/settings/settingsIdle.png");
+    Texture2D buttonCreditsIdle = LoadTexture("../assets/buttons/credits/creditsIdle.png");
+    Texture2D buttonCreditsHover = LoadTexture("../assets/buttons/credits/creditsHover.png");
+    Texture2D buttonQuitIdle = LoadTexture("../assets/buttons/quit/quitIdle.png");
     
     SetTargetFPS(24);
 
@@ -94,13 +97,67 @@ void drawmainMenu(gameState& gameState)
         }
 
         DrawTexture(texScarfyAnim, GetScreenWidth() / 2 - texScarfyAnim.width / 2, 0, WHITE);
-        DrawTexture(buttonPlayIdle, 90, 250, WHITE);
-        DrawTexture(buttonSettingsIdle, 88, 330, WHITE);
-        DrawTexture(buttonCreditsIdle, 89, 410, WHITE);
-        DrawTexture(buttonQuitIdle, 90, 570, WHITE);
+        //DrawTexture(buttonPlayIdle, 90, 250, WHITE);
+        Rectangle buttonPlayIdleRec = { 90, 250, 170, 60 };
+        Color buttonPlayIdleColor = { 255, 255, 255, 0 };
+        DrawTextureRec(buttonPlayIdle, buttonPlayIdleRec, Vector2{ 170, 60 }, buttonPlayIdleColor);
+
+        //DrawTexture(buttonSettingsIdle, 88, 330, WHITE);
+        Rectangle buttonSettingsIdleRec = { 88, 330, 343, 60 };
+        Color buttonSettingsIdleColor = { 255, 255, 255, 0 };
+        DrawTextureRec(buttonSettingsIdle, buttonSettingsIdleRec, Vector2{ 343, 60 }, buttonSettingsIdleColor);
+
+        //DrawTexture(buttonCreditsIdle, 89, 410, WHITE);
+        Rectangle buttonCreditsIdleRec = { 89, 410, 300, 60 };
+        Color buttonCreditsIdleColor = { 255, 255, 255, 0 };
+        DrawTextureRec(buttonCreditsIdle, buttonCreditsIdleRec, Vector2{ 300, 60 }, buttonCreditsIdleColor);
+
+        //DrawTexture(buttonQuitIdle, 90, 570, WHITE);
+
+        if (CheckCollisionPointRec(GetMousePosition(), buttonPlayIdleRec))
+        {
+            DrawTexture(buttonPlayHover, 90, 250, WHITE);
+            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+            {
+                gameState = GAME;
+                UnloadTexture(texScarfyAnim);
+                UnloadImage(imScarfyAnim);
+                ToggleBorderlessWindowed();
+            }
+        }
+        else
+        {
+            DrawTexture(buttonPlayIdle, 90, 247, WHITE);
+        }
+
+        if (CheckCollisionPointRec(GetMousePosition(), buttonSettingsIdleRec))
+        {
+            DrawTexture(buttonSettingsHover, 88, 333, WHITE);
+            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+            {
+                //Button Function
+            }
+        }
+        else
+        {
+            DrawTexture(buttonSettingsIdle, 88, 330, WHITE);
+        }
+
+        if (CheckCollisionPointRec(GetMousePosition(), buttonCreditsIdleRec))
+        {
+            DrawTexture(buttonCreditsHover, 89, 410, WHITE);
+            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+            {
+                //Button Function
+            }
+        }
+        else
+        {
+            DrawTexture(buttonCreditsIdle, 89, 407, WHITE);
+        }
+
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 }
 
