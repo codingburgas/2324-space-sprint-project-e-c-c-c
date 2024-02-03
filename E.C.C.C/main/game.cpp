8c812f52268f23dc0d6ae0c92eb17ec596bc5713
NEKalashnik22 <NEKalashnik22@codingburgas.bg>
2024-02-03 22:22:37 +0200

    Add player name input page (Close #26)

--- E.C.C.C/main/game.cpp
+++ E.C.C.C/main/game.cpp
@@ -1,50 +1,113 @@
-#include "game.hpp"
+﻿
+#include "raylib.h"
+#define MAX_INPUT_CHARS     9
 
-
-
-void game()
+int main(void)
 {
 
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 1920;
-    const int screenHeight = 1020;
+    const int screenHeight = 1040;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+    char name[MAX_INPUT_CHARS + 1] = "\0";
+    int letterCount = 0;
 
-    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+    Rectangle textBox = { screenWidth / 2.0f - 270 , 835 , 450, 110 };
+    bool mouseOnText = false;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    int framesCounter = 0;
+    InitWindow(screenWidth, screenHeight, "test");
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+
+    Texture2D texture = LoadTexture("../assets/contractTerminal.png");
+
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
-        //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        int key = GetCharPressed();
+
+        while (key > 0)
+        {
+
+            if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS))
+            {
+                name[letterCount] = (char)key;
+                name[letterCount + 1] = '\0';
+                letterCount++;
+            }
+
+            key = GetCharPressed();
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE))
+        {
+            letterCount--;
+            if (letterCount < 0) letterCount = 0;
+            name[letterCount] = '\0';
+        }
+
+        else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+
+        if (mouseOnText) framesCounter++;
+        else framesCounter = 0;
+
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+        DrawTexture(texture, screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2 - 10, WHITE);
 
-        DrawCircleV(ballPosition, 50, MAROON);
+        DrawRectangleRec(textBox, LIGHTGRAY);
+        if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGREEN);
+        else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, BLACK);
+
+        DrawText(name, (int)textBox.x + 5 * 2, (int)textBox.y + 16, 80, BLACK);
+
+        DrawText(TextFormat("INPUT CHARS: ", letterCount, MAX_INPUT_CHARS), 720, 950, 40, DARKGRAY);
+
+        switch (letterCount)
+        {
+        case 7:
+            DrawText(TextFormat("%i", letterCount, MAX_INPUT_CHARS), 1040, 950, 40, YELLOW);
+            break;
+
+        case 8:
+            DrawText(TextFormat("%i", letterCount, MAX_INPUT_CHARS), 1040, 950, 40, ORANGE);
+            break;
+        case 9:
+            DrawText(TextFormat("%i", letterCount, MAX_INPUT_CHARS), 1040, 950, 40, RED);
+            DrawText("Press BACKSPACE to delete chars...", 550, 780, 40, DARKGRAY);
+            break;
+        default:
+            DrawText(TextFormat("%i", letterCount, MAX_INPUT_CHARS), 1040, 950, 40, DARKGRAY);
+            break;
+        }
+
+        DrawText(TextFormat("/9", letterCount, MAX_INPUT_CHARS), 1065, 950, 40, DARKGRAY);
+        if (letterCount < MAX_INPUT_CHARS)
+        {
+            if (((framesCounter / 20) % 2) == 0) DrawText("_", (int)textBox.x + 8 * 2 + MeasureText(name, 40 * 2), (int)textBox.y + 12 * 2, 40 * 2, DARKGREEN);
+        }
+
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
+
+    }
+    UnloadTexture(texture);
+    CloseWindow();
+
+    bool IsAnyKeyPressed();
+    {
+        bool keyPressed = false;
+        int key = GetKeyPressed();
+
+        if ((key >= 32) && (key <= 126)) keyPressed = true;
+
+        return keyPressed;
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
 }
+
+
+
+
+
