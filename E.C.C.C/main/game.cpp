773e489982d54ca03da8091fa607ae2ac6ae8e18
BRMilev22 <BRMilev22@codingburgas.bg>
2024-02-05 14:34:30 +0200

    Create game menu #31

--- E.C.C.C/main/game.cpp
+++ E.C.C.C/main/game.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "game.hpp"
 
 void game()
@@ -5,23 +6,63 @@ void game()
     const int screenWidth = GetScreenWidth();
     const int screenHeight = GetScreenHeight();
 
-    Texture2D background = LoadTexture("../assets/gameBg.png");
+    // Texture2D background = LoadTexture("../assets/gameBg.png");
+
+    const float sunRadius = 40.0f;
+    float earthRadius = 6.0f;
+    float earthOrbitRadius = 80.0f;
+    const float moonRadius = 2.0f;
+    float moonOrbitRadius = 15.0f;
+
+    SetTargetFPS(60);
+
+    float rotationSpeed = 0.2f;
+    float rotationAngle = 0.0f;
+    float zoomLevel = 1.0f;
+
+    Color earthColor = BLUE;
+    bool isEarthClicked = false;
 
     while (!WindowShouldClose())
     {
+        rotationAngle += rotationSpeed;
+
+        zoomLevel += GetMouseWheelMove() * 0.1f;
+        zoomLevel = fmaxf(zoomLevel, 0.1f);
+
+        earthRadius = 6.0f * zoomLevel;
+        earthOrbitRadius = 80.0f * zoomLevel;
+        moonOrbitRadius = 15.0f * zoomLevel;
+
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
+        // DrawTexture(background, screenWidth / 2 - background.width / 2, screenHeight / 2 - background.height / 2 - 10, WHITE);
 
-        DrawTexture(background, screenWidth / 2 - background.width / 2, screenHeight / 2 - background.height / 2 - 10, WHITE);
+        DrawCircleV(Vector2{ (float)screenWidth / 2, (float)screenHeight / 2 }, sunRadius * zoomLevel, GOLD);
+
+        DrawCircleLines(screenWidth / 2, screenHeight / 2, earthOrbitRadius, Fade(WHITE, 0.5f));
+
+        float earthX = screenWidth / 2 + earthOrbitRadius * cos(DEG2RAD * (5.0f + rotationAngle));
+        float earthY = screenHeight / 2 + earthOrbitRadius * sin(DEG2RAD * (5.0f + rotationAngle));
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointCircle(GetMousePosition(), Vector2{ earthX, earthY }, earthRadius))
+        {
+            isEarthClicked = !isEarthClicked;
+            if (isEarthClicked)
+            {
+                earthColor = RED;
+            }
+            else
+            {
+                earthColor = BLUE;
+            }
+        }
+
+        DrawCircleV(Vector2{ earthX, earthY }, earthRadius, earthColor);
 
         EndDrawing();
-
-        if (IsKeyPressed(KEY_ESCAPE))
-        {
-            UnloadTexture(background);
-            SetTargetFPS(24);
-        }
     }
-    UnloadTexture(background);
+
+    CloseWindow();
 }
