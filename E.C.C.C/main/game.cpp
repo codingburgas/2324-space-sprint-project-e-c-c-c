c494d31bcd7bc4b99d382c0d67b956cee4423f4c
BRMilev22 <BRMilev22@codingburgas.bg>
2024-02-07 08:00:21 +0200

    Add stars to game background

--- E.C.C.C/main/game.cpp
+++ E.C.C.C/main/game.cpp
@@ -71,6 +71,19 @@ void game()
         playerName.close();
     }
 
+    std::vector<Vector2> starPositions;
+    std::vector<Vector2> starVelocities;
+
+    // Spawn stars
+    for (int i = 0; i < 200; ++i) 
+    {
+        Vector2 position = { (float)GetRandomValue(0, screenWidth), (float)GetRandomValue(0, screenHeight) };
+        Vector2 velocity = { (float)GetRandomValue(-50, 50), (float)GetRandomValue(-50, 50) };
+        starPositions.push_back(position);
+        starVelocities.push_back(velocity);
+    }
+
+
     while (!WindowShouldClose())
     {
         mercuryRotationAngle += mercuryRotationSpeed;
@@ -116,10 +129,31 @@ void game()
             levelsFile.close();
         }
 
+        for (size_t i = 0; i < starPositions.size(); ++i) 
+        {
+            // Move the stars faster
+            starPositions[i].x += starVelocities[i].x * GetFrameTime() * 0.1f;
+            starPositions[i].y += starVelocities[i].y * GetFrameTime() * 0.1f;
+
+            // Wrap around screen edges
+            if (starPositions[i].x > screenWidth) starPositions[i].x = 0;
+            else if (starPositions[i].x < 0) starPositions[i].x = screenWidth;
+            if (starPositions[i].y > screenHeight) starPositions[i].y = 0;
+            else if (starPositions[i].y < 0) starPositions[i].y = screenHeight;
+        }
+
+
         BeginDrawing();
 
         ClearBackground(BLACK);
         // DrawTexture(background, screenWidth / 2 - background.width / 2, screenHeight / 2 - background.height / 2 - 10, WHITE);
+
+        // Draw stars
+        for (size_t i = 0; i < starPositions.size(); ++i) 
+        {
+            DrawPixelV(starPositions[i], WHITE);
+        }
+
 
         DrawCircleV(Vector2{ (float)screenWidth / 2, (float)screenHeight / 2 }, sunRadius * zoomLevel, GOLD);
 
